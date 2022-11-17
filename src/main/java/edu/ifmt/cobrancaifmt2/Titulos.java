43bfd544a9ba9736d60ec2c2beb9f07602b13c6d
Tiago Guimarães Pinto <tguimaraespinto@gmail.com>
2022-11-16 21:34:07 -0400

    Add files via upload

---
+++ src/main/java/edu/ifmt/cobrancaifmt2/Titulos.java
@@ -0,0 +1,10 @@
+package edu.ifmt.cobrancaifmt2;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import edu.ifmt.cobrancaifmt2.model.Titulo;
+
+public interface Titulos extends JpaRepository<Titulo, Long> {
+
+}
